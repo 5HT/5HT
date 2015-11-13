60c37d48301433469877a9f3fd1fb4d66f2fb850
Namdak Tonpa <maxim@synrc.com>
2015-11-13 05:42:26 +0200

    exe

--- exe.lean
+++ exe.lean
@@ -1,61 +1,64 @@
 import data.nat data.bool data.prod data.list
 namespace Exe open nat bool prod list classical
 
-      -- basic i/o protocol
-      -- storing computation results in separate lists inside application signatures
+      -- basic protocol
 
-      structure ok    (A : Type)            := (val: A)
-      structure error (A : Type)            := (val: A)
-      structure io    (A : Type) (B : Type) := (exe: A) (val: B)
-      inductive proto (O : Type) (E : Type) : Type :=
+      structure ok    (A : Type)            := (val : A)
+      structure error (A : Type)            := (val : A)
+      structure io    (A : Type) (B : Type) := (exe : A) (val : B)
+      inductive proto (O : Type) (E : Type) : Type  :=
                 | ok     : O     → proto O E
                 | error  : E     → proto O E
                 | io     : O → E → proto O E
 
-      -- high level basic kv storage interface
-
-      structure kv [class] (T K V: Type) :=
-                (get : Π (t: T) (k: K) (v: V), T → K → proto V V)
-                (put : Π (t: T) (k: K) (v: V), T → K → V → proto V V)
-
-      -- interface for stores with compound keys
-
-      structure indexed [class] (T K V: Type)
-        extends kv T K V := (index : Π (t: T) (k: K) (v: V), T → K → proto V V)
-
-      -- table metainfo persistent container state and iterator cursor
+      -- persistent types
 
       structure table (T: Type) := (tab : T) (attr : list string) (keys : list string)
       structure container       := (top : ℕ) (size : ℕ)
       structure iterator        := (id  : ℕ) (next : ℕ) (prev : ℕ)
+      structure subscription    := (who : ℕ) (whom : ℕ) (how  : Type)
+      structure user extends iterator := (names : list string) (surnames : list string)
+      structure task  := (name   : string)
+      structure event := (name   : string)
+      structure flow  := (source : task) (target : task)
+      inductive docs  : Type :=
+                | subscription : ℕ → ℕ → Type                 → docs
+                | user         : list string → list string    → docs
+                | process      : list task → list flow → list event → docs → docs 
 
-      check @table.mk
+      -- basic kv store
+
+      structure kv [class] (T : Type) (P : Type) :=
+                (get : Π (t : T), T → ℕ    → P)
+                (put : Π (t : T), T → docs → P)
+
+      -- extended store with secondary indexes
+
+      structure indexed [class] (T: Type) (P : Type)
+        extends kv T P := (index : Π (t : ℕ), list ℕ → list P)
 
       -- feed server state
 
-      structure feed [class] (I C K V: Type)
-        extends kv I K V := (container : table C)
-                            (iterator  : table I)
-                            (add : Π (i: I) (v: V), I → V → proto V V)
-                            (rem : Π (i: I) (v: V), I → V → proto V V)
+      structure feed [class] (C T: Type) (P: Type)
+        extends kv T P := (container : table C)
+                          (iterator  : table T)
+                          (errors    : list (error P))
+                          (add       : Π (d: docs), docs → P)
+                          (rem       : Π (d: docs), docs → P)
 
       -- process server state
 
-      structure process (docs : Type) (tasks flows events : Type) (P O E : Type)
-        extends table P,
-                iterator := (env : list docs)
-                            (tax : list (prod events (prod tasks flows)))
-                            (io  : list (io O E))
+      structure process (tasks flows events : Type) (S P E : Type)
+        extends iterator := (env   : list docs)
+                            (feed  : feed container P P)
+                            (def   : prod (prod tasks flows) events)
+                            (io    : list (io P E))
+                            (spawn : P → S)
+                            (run   : S → P)
+                            (get   : unit → S → S)
+                            (act   : P → S → S)
 
-      check @process.tab
-
-      -- applications' state and protocol
-
-      structure BPE [class] (S : Type) (P: Type) := 
-                (spawn  : P → S)
-                (run    : S → P)
-                (get    : P → S → S)
-                (step   : P → S → S)
+      -- application state and protocol
 
       structure N2O [class] (S : Type) (P: Type) :=
                 (spawn  : P → S)
@@ -63,11 +66,11 @@ namespace Exe open nat bool prod list classical
                 (get    : P → S → S)
                 (handle : P → S → S)
 
-      -- storage's table and result
-
-                check @N2O.rec
-
+      check @process.act
+      check @N2O.rec
       check @feed.add
+      check @table.mk
+
 
 end Exe
 
