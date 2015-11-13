5e6c94cacd46105627d6c15454dc220f90d76af5
Namdak Tonpa <maxim@synrc.com>
2015-11-13 06:18:59 +0200

    exe

--- exe.lean
+++ exe.lean
@@ -3,60 +3,68 @@ namespace Exe open nat bool prod list classical
 
       -- basic protocol
 
-      structure ok    (A : Type)            := (val : A)
-      structure error (A : Type)            := (val : A)
-      structure io    (A : Type) (B : Type) := (exe : A) (val : B)
-      inductive proto (O : Type) (E : Type) : Type  :=
-                | ok     : O     → proto O E
-                | error  : E     → proto O E
-                | io     : O → E → proto O E
+      structure ok    (A: Type)           := (val: A)
+      structure error (A: Type)           := (val: A)
+      structure io    (A: Type) (B: Type) := (exe: A) (val: B)
+      inductive proto (O: Type) (E: Type) :=
+                                          | ok: O → proto O E
+                                          | error: E → proto O E
+                                          | io: O → E → proto O E
 
-      -- persistent types
+      -- simple documents
 
-      structure table (T: Type) := (tab : T) (attr : list string) (keys : list string)
-      structure container       := (top : ℕ) (size : ℕ)
-      structure iterator        := (id  : ℕ) (next : ℕ) (prev : ℕ)
-      structure subscription    := (who : ℕ) (whom : ℕ) (how  : Type)
-      structure user extends iterator := (names : list string) (surnames : list string)
-      structure task  := (name   : string)
-      structure event := (name   : string)
-      structure flow  := (source : task) (target : task)
-      inductive docs  : Type :=
-                | subscription : ℕ → ℕ → Type                 → docs
-                | user         : list string → list string    → docs
-                | process      : list task → list flow → list event → docs → docs 
+      structure task            := (name: string)
+      structure event           := (name: string)
+      structure flow            := (source: task) (target: task)
+      structure subscription    := (who: ℕ) (whom: ℕ) (how: Type)
 
-      -- basic kv store
+      -- metainfo
 
+      structure table (T: Type) := (tab: T) (attr: list string) (keys: list string)
+      structure id_seq          := (thing: string) (id: ℕ)
+
+      -- all registered documents
+
+      inductive docs  :=
+                      | subscription: ℕ → ℕ → Type → docs
+                      | user: list string → list string → docs
+                      | process: list task → list flow → list event → docs → docs
+
+      -- store
+ 
       structure kv [class] (T : Type) (P : Type) :=
-                (get : Π (t : T), T → ℕ    → P)
-                (put : Π (t : T), T → docs → P)
-
-      -- extended store with secondary indexes
+                (get: Π (t: T), T → ℕ    → P)
+                (put: Π (t: T), T → docs → P)
 
       structure indexed [class] (T: Type) (P : Type)
         extends kv T P := (index : Π (t : ℕ), list ℕ → list P)
 
+      -- persistent traversable
+
+      structure container                             := (id: ℕ) (top: ℕ) (size: ℕ)
+      structure iterator (T: Type) extends kv T T     := (type: T) (feed: container) (id: ℕ) (next: ℕ) (prev: ℕ)
+      structure user     (T: Type) extends iterator T := (names: list string) (surnames: list string)
+
       -- feed server state
 
-      structure feed [class] (C T: Type) (P: Type)
-        extends kv T P := (container : table C)
-                          (iterator  : table T)
-                          (errors    : list (error P))
-                          (add       : Π (d: docs), docs → P)
-                          (rem       : Π (d: docs), docs → P)
+      structure feed [class] (T: Type) (S: Type) 
+        extends iterator T,
+                container := (errors    : list (error S))
+                             (add       : Π (d: docs), docs → S)
+                             (remove    : Π (d: docs), docs → S)
 
       -- process server state
 
       structure process (tasks flows events : Type) (S P E : Type)
-        extends iterator := (env   : list docs)
-                            (feed  : feed container P P)
-                            (def   : prod (prod tasks flows) events)
-                            (io    : list (io P E))
-                            (spawn : P → S)
-                            (run   : S → P)
-                            (get   : unit → S → S)
-                            (act   : P → S → S)
+        extends iterator P := (env: list docs)
+                              (container: container)
+                              (feed_id: ℕ)
+                              (taxonomy: prod (prod tasks flows) events)
+                              (io: list (io P E))
+                              (spawn: P → S)
+                              (run:   S → P)
+                              (retrive: unit → S → S)
+                              (action:     P → S → S)
 
       -- application state and protocol
 
@@ -66,7 +74,7 @@ namespace Exe open nat bool prod list classical
                 (get    : P → S → S)
                 (handle : P → S → S)
 
-      check @process.act
+      check @process.action
       check @N2O.rec
       check @feed.add
       check @table.mk
