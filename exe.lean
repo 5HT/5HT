6d3b031189faf0ea9e5c6f66f6ad58f1577f7300
Namdak Tonpa <maxim@synrc.com>
2015-11-13 04:54:15 +0200

    exe

--- exe.lean
+++ exe.lean
@@ -1,60 +1,73 @@
 import data.nat data.bool data.prod data.list
 namespace Exe open nat bool prod list classical
 
-      structure OK    (A: Type) := mk :: (dat: A)
-      structure ERROR (A: Type) := mk :: (dat: A)
-      structure IO    (A: Type)
-                      (B: Type) := mk :: (eva: A) (dat: B)
+      -- basic i/o protocol
+      -- storing computation results in separate lists inside application signatures
 
-      inductive PROTO (O : Type) : Type :=
-                | ok     : OK    O    → PROTO O
-                | error  : ERROR O    → PROTO O
-                | io     : IO    O O  → PROTO O
+      structure ok    (A : Type)            := (val: A)
+      structure error (A : Type)            := (val: A)
+      structure io    (A : Type) (B : Type) := (exe: A) (val: B)
+      inductive proto (O : Type) (E : Type) : Type :=
+                | ok     : O     → proto O E
+                | error  : E     → proto O E
+                | io     : O → E → proto O E
 
-      structure BPE [class] (S : Type) (P: Type) :=
-                (spawn  : S → S)
-                (run    : S → S)
+      -- high level basic kv storage interface
+
+      structure kv [class] (T K V: Type) :=
+                (get : Π (t: T) (k: K) (v: V), T → K → proto V V)
+                (put : Π (t: T) (k: K) (v: V), T → K → V → proto V V)
+
+      -- interface for stores with compound keys
+
+      structure indexed [class] (T K V: Type)
+        extends kv T K V := (index : Π (t: T) (k: K) (v: V), T → K → proto V V)
+
+      -- table metainfo persistent container state and iterator cursor
+
+      structure table (T: Type) := (tab : T) (attr : list string) (keys : list string)
+      structure container       := (top : ℕ) (size : ℕ)
+      structure iterator        := (id  : ℕ) (next : ℕ) (prev : ℕ)
+
+      check @table.mk
+
+      -- feed server state
+
+      structure feed [class] (I C K V: Type)
+        extends kv I K V := (container : table C)
+                            (iterator  : table I)
+                            (add : Π (i: I) (v: V), I → V → proto V V)
+                            (rem : Π (i: I) (v: V), I → V → proto V V)
+
+      -- process server state
+
+      structure process (docs : Type) (tasks flows events : Type) (P O E : Type)
+        extends table P,
+                iterator := (env : list docs)
+                            (tax : list (prod events (prod tasks flows)))
+                            (io  : list (io O E))
+
+      check @process.tab
+
+      -- applications' state and protocol
+
+      structure BPE [class] (S : Type) (P: Type) := 
+                (spawn  : P → S)
+                (run    : S → P)
                 (get    : P → S → S)
-                (action : P → S → S)
+                (step   : P → S → S)
 
-      structure N2O [class] (S  : Type) (P: Type) :=
-                (spawn  : S → S)
-                (run    : S → S)
+      structure N2O [class] (S : Type) (P: Type) :=
+                (spawn  : P → S)
+                (run    : S → P)
                 (get    : P → S → S)
-                (event  : P → S → S)
+                (handle : P → S → S)
+
+      -- storage's table and result
 
                 check @N2O.rec
 
-      structure KV [class] (T K V: Type) :=
-                mk :: (get: T → K → V)
-                      (set: T → K → V)
-
-      structure KVS (T K V: Type) extends KV T K V :=
-                mk :: (add: T → K → V)
-                      (rem: T → K → V)
-
-      structure Container [class] (A: Type) :=
-                mk :: (top: A)
-                      (count: nat)
-
-      structure Iterator [class] (A T: Type) :=
-                mk :: (id:   nat)
-                      (next: A)
-                      (prev: A)
-                      (time: A)
-                      (cons: Π A: Type, A → Container A → Container A)
-
-      structure ITER [class] (A: Type) :=
-                      (T: Type)
-                      (id:   nat)
-                      (next: A)
-                      (prev: A)
-                      (time: T)
-                      (cons: Π A: Type, A → Container A → Container A)
-
-
-      check ITER.rec
-      check @Iterator.cons
+      check @feed.add
 
 end Exe
 
