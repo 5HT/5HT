1d0817796c72d93cefad035f20c18e979a103902
Namdak Tonpa <maxim@synrc.com>
2015-11-20 08:14:04 +0200

    pngs

--- exe/application.lean
+++ exe/application.lean
@@ -2,7 +2,7 @@
       import data.nat data.bool data.prod data.list data.sum data.stream
              data.unit data.uprod data.tuple data.option
         open nat bool prod sum list classical option stream
-   namespace application 
+   namespace application
 
       record app (P S: Type) := (spawn  : P → S)
                                 (run    : S → P)
