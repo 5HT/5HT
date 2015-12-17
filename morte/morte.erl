2fe049a8aa8402ef95be3b26ed6ea25384a0291b
Namdak Tonpa <maxim@synrc.com>
2015-12-17 14:54:17 +0200

    initial parser

--- morte/morte.erl
+++ morte/morte.erl
@@ -6,13 +6,18 @@
                      C =:= $+; C =:= $-; C =:= $*; C =:= $/; C =:= $.; C =:= $\\;C =:= $);
                      C =:= $<; C =:= $>; C =:= $=; C =:= $|; C =:= $^; C =:= $~; C =:= $@).
 
-tokens(<<>>,                  _, {_,_}, Acc) -> lists:reverse(Acc);
-tokens(<<$\n,     R/binary>>, L, {_,_}, Acc) -> tokens(R,L+1,{1,[]},   Acc);
-tokens(<<$(,      R/binary>>, L, {t,C}, Acc) -> tokens(R,L,{t,[$(]},   Acc);
+stack(C,Acc) -> case lists:flatten(C) of [] -> Acc; Re -> lists:flatten([{token,lists:reverse(Re)}|Acc]) end.
+label(C,Acc) -> case lists:flatten(C) of [] -> Acc; Re -> lists:flatten([{token,{label,lists:reverse(Re)}}|Acc]) end.
+read() -> {ok,Bin} = file:read_file("cat2.txt"), io:format("~ts~n",[unicode:characters_to_list(Bin)]), tokens(Bin,0,{1,[]},[]).
+main() -> parse(read(),[]).
+
+tokens(<<>>,                  _, {_,C}, Acc) -> lists:reverse(stack(C,Acc));
+tokens(<<$\n,     R/binary>>, L, {_,C}, Acc) -> tokens(R,L+1,{1,[]},   stack(C,Acc));
+tokens(<<$(,      R/binary>>, L, {t,_}, Acc) -> tokens(R,L,{t,[$(]},   Acc);
 tokens(<<$),      R/binary>>, L, {t,C}, Acc) -> tokens(R,L,{t,[$)|C]}, Acc);
-tokens(<<$(,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},[{token,"("}|stack(C,Acc)]);
-tokens(<<$),      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},[{token,")"}|stack(C,Acc)]);
-tokens(<<$:,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{1,[]},[{token,":"}|stack(C,Acc)]);
+tokens(<<$(,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},[{token,open}|stack(C,Acc)]);
+tokens(<<$),      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},[{token,close}|stack(C,Acc)]);
+tokens(<<$:,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{1,[]},[{token,colon}|label(C,Acc)]);
 tokens(<<$*,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{1,[]},[{token,star}|stack(C,Acc)]);
 tokens(<<"→"/utf8,R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{1,[]},[{token,arrow}|stack(C,Acc)]);
 tokens(<<"λ"/utf8,R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{1,[]},[{token,lambda}|stack(C,Acc)]);
@@ -23,5 +28,28 @@ tokens(<<X,       R/binary>>, L, {t,C}, Acc) when ?is_termi(X) -> tokens(R,L,{t,
 tokens(<<X,       R/binary>>, L, {_,C}, Acc) when ?is_termi(X) -> tokens(R,L,{t,[X]},  stack(C,[Acc]));
 tokens(<<X,       R/binary>>, L, {_,C}, Acc) when ?is_space(X) -> tokens(R,L,{s,[C]},  Acc).
 
-stack(C,Acc) -> case lists:flatten(C) of [] -> Acc; Re -> lists:flatten([{token,lists:reverse(Re)}|Acc]) end.
-read() -> {ok,Bin} = file:read_file("fun.txt"), tokens(Bin,0,{1,[]},[]).
+parse([],Acc)                      -> {[],lists:reverse(Acc)};
+parse([{token,open}|T],Acc)        -> parse_closure(T,Acc);
+parse([{token,star}|T],Acc)        -> parse(T,[{const,star}|Acc]);
+%parse([{token,lambda}|T],[{var,X}|Acc]) -> parse([],[{apply,[{var,X}],element(2,parse_arrow(T,Acc))}]);
+parse([{token,lambda}|T],Acc)      -> parse_arrow(T,Acc);
+parse([{token,pi}|T],Acc)          -> parse_arrow(T,Acc);
+parse([{token,arrow}|T],Acc)       -> {tail(T),[{arrow,element(2,parse([],Acc)),element(2,parse(T,[]))}]};
+parse([{token,close}|_]=All,Acc)   -> {All,Acc};
+parse([{token,L}|T],[{var,X}|Acc]) -> parse([],[{apply,[{var,X}],element(2,parse(T,[{var,L}|Acc]))}]);
+parse([{token,L}|T],Acc)           -> parse(T,[{var,L}|Acc]);
+parse(T,Acc)                       -> {T,Acc}.
+
+parse_closure(Tail,Acc) ->
+    {[{token,close}|Out],In} = parse(Tail,[]),
+    {Out,[{closure,In}|Acc]}.
+
+parse_arrow([{token,open},{token,{label,Label}},{token,colon}|Tail],Acc) ->
+    {[{token,close},{token,arrow}|Out],In} = parse(Tail,Acc),
+    {[],[{lambda,Label,In,element(2,parse(Out,Acc))}|Acc]}.
+
+tail([]) -> [];
+tail(Tl) -> tl(Tl).
+
+head([]) -> [];
+head(Tl) -> hd(Tl).
