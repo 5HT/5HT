be2277304472255efb315f90edffe6bf5f25f862
Namdak Tonpa <maxim@synrc.com>
2015-12-19 02:17:14 +0200

    milestone

--- morte/morte.erl
+++ morte/morte.erl
@@ -31,7 +31,7 @@ stack(C,Acc) ->
         [X|Aa] when ?is_alpha(X) -> lists:flatten([{name,lists:reverse([X|Aa])}|Acc]);
         Re -> lists:flatten([list_to_atom(lists:reverse(Re))|Acc]) end.
 
-read() -> {ok,Bin} = file:read_file("fun.txt"), io:format("~ts~n",[unicode:characters_to_list(Bin)]), tokens(Bin,0,{1,[]},[]).
+read() -> {ok,Bin} = file:read_file("cat.txt"), io:format("~ts~n",[unicode:characters_to_list(Bin)]), tokens(Bin,0,{1,[]},[]).
           main() -> expr(read(),[]).
 
 %    EXPR := "lambda" "(" LABEL ":" EXPR ")" "arrow" EXPR => {LAM,[{NAME:LABEL,I:EXPR, O;EXPR]}
@@ -50,7 +50,7 @@ read() -> {ok,Bin} = file:read_file("fun.txt"), io:format("~ts~n",[unicode:chara
 expr([],Acc)                                     -> {[],Acc};
 expr([Fun,open,{name,Label},colon|T],Acc) when Fun == pi; Fun == lambda -> {T1,Acc1} = expr(T,Acc),
                                                         expr(T,[{domain,{Label,Acc1,Fun}}|Acc]);
-expr([{name,L}|T],[{Name,X}|Acc]) when Name /= arrow -> expr(T,[{app,{[{Name,X}],[{var,L}]}}|Acc]);
+expr([{name,L}|T],[{Name,X}|Acc]) when Name /= arrow -> expr(T,[{apply,{[{Name,X}],[{var,L}]}}|Acc]);
 expr([{name,L}|T],[{arrow},{Name,X}|Acc])            -> expr(T,[{arrow,{[{Name,X}],[{var,L}]}}|Acc]);
 expr([{name,L}|T],Acc)                               -> expr(T,[{var,L}|Acc]);
 expr(T,[{arrow},{Name,X}|Acc])                   -> expr(T,[{arrow,{[{Name,X}],element(2,expr(T,Acc))}}|Acc]);
@@ -58,11 +58,17 @@ expr([star|T],[{arrow},{Name,X}|Acc])            -> expr(T,[{arrow,{[{Name,X}],[
 expr([star|T],Acc)                               -> expr(T,[{const,star}|Acc]);
 expr([close,arrow|T],[{Name,X},{domain,{Label,A,Fun}}|Acc]) -> {T1,Acc1} = expr(T,Acc),
                                                     expr(T1,[{Fun,{[{name,Label}],A,element(2,expr(T,Acc))}}|Acc]);
-expr([close,{name,L}|T],[{Name,X}|Acc]=A)        -> expr(T,[{app,{[{Name,X}],[{var,L}]}}|Acc]);
-expr([close|T],Acc)                              -> expr([],Acc);
-expr([open    |T],Acc)                           -> {T1,Acc1} = expr(T,Acc),
-                                                    expr(T1,lists:flatten([Acc1|Acc]));
+expr([close,{name,L}|T],[{Name,X}|Acc]=A)        -> expr(T,[{apply,{[{Name,X}],[{var,L}]}}|Acc]);
+expr([close|T],Acc)                              -> rewind([],T,lists:reverse(Acc));
+expr([open    |T],Acc)                           -> expr(T,[{open}|Acc]);
 expr([arrow   |T],Acc)                           -> expr(T,[{arrow}|Acc]).
+
+rewind([] ,           T,[{Name,X}|Rest])           -> rewind([{Name,X}],T,Rest);
+rewind(Acc,           T,[{open}|Rest])             -> {T,[Acc|Rest]};
+rewind(Acc,           T,[])                        -> {T,Acc};
+rewind(Acc,           T,[X])                       -> {T,[X]};
+rewind([{Name,X}|Acc],T,[{Coname,Y}|Rest])         -> rewind([{apply,{[{Name,X}],[{Coname,Y}]}}|Acc],T,Rest);
+rewind([{Name,X}|Acc],T,[{arrow},{Coname,Y}|Rest]) -> rewind([{arrow,{[{Name,X}],[{Coname,Y}]}}|Acc],T,Rest).
 
 tail([]) -> [];
 tail(Tl) -> tl(Tl).
